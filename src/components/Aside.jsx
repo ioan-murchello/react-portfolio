60f955a458d0b1a55d28d3194328478e471458df
ioan-murchello <mihalkurz@gmail.com>
2025-06-12 19:18:09 +0200

    added dvh to resolve small bug in mainSidebar

--- src/components/Aside.jsx
+++ src/components/Aside.jsx
@@ -5,9 +5,9 @@ const Aside = ({ isOpen, children }) => {
     fixed  min-w-fit inset-y-0 left-0 z-10
     bg-white transition-all duration-500 ease-in-out
     flex flex-col justify-start items-center
-    overflow-hidden p-6
+    overflow-y-auto p-6 w-full h-dvh
 
-    ${isOpen ? "w-full h-dvh translate-x-0" : "w-0 -translate-x-full"}
+    ${isOpen ? "translate-x-0" : "w-0 -translate-x-full"}
 
     lg:static lg:w-auto lg:h-auto lg:translate-x-0 lg:p-6 lg:flex lg:bg-white  
   `}
